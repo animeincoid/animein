c6e499779bdcc191157fba8431aab7ee1c8424fe
animeincoid <155032347+animeincoid@users.noreply.github.com>
2023-12-27 17:20:07 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+function navSlide() {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
+
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.5
+        }s`;
+      }
+    });
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+}
+
+navSlide();
+
+function kirimData() {
+    const password = document.getElementById("password").value;
+
+    const pass = ("hunterxhunter");
+    
+    if (password == ""){
+
+    }else if (password != pass ) {
+ 
+    }else {
+        alert("mencari" , document.getElementById("hxh").click());
+    }
+}
